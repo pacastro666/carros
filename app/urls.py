966bea4b3247e5dc029ebab4fbac9779c1ff36d1
pacastro666@gmail.com <pauloantunes66@hotmail.com>
2024-08-11 16:52:36 -0300

    adicionado template com css e buscao

--- app/urls.py
+++ app/urls.py
@@ -20,7 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_view
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars',cars_view)
+    path('cars',cars_view,name='cars_list')
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
