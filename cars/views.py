911be3f1c03ea75761f25564b0ea2445c76891ae
pacastro666@gmail.com <pauloantunes66@hotmail.com>
2024-08-13 23:01:31 -0300

    formulario de cadastro do django

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from cars.models import Car
+from cars.forms import CarForm
 
 
 
@@ -14,3 +15,17 @@ def cars_view(request):
         'cars.html',
         {'cars':cars}
     )
+
+
+
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST,request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+        
+    else:
+       new_car_form = CarForm()
+    return render(request,'new_car.html', {'new_car_form': new_car_form})
