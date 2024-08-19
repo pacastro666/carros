dcf600fc2fb0ee9f5818c8bc2ca64f0c74a83742
pacastro666@gmail.com <pauloantunes66@hotmail.com>
2024-08-18 23:36:32 -0300

    reescrevendo a view de cars com classes

--- app/urls.py
+++ app/urls.py
@@ -19,14 +19,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from accounts.views import login_view, logout_view, register_view
-from cars.views import cars_view,new_car_view
+from cars.views import NewCarView,CarsView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
-    path('cars',cars_view,name='cars_list'),
+    path('cars',CarsView.as_view(),name='cars_list'),
     path('login/',login_view,name='login'),
     path('logout/',logout_view,name='logout'),
-    path('new_car/',new_car_view,name='new_car'),
+    path('new_car/',NewCarView.as_view(),name='new_car'),
     path('register/',register_view,name='register'),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
